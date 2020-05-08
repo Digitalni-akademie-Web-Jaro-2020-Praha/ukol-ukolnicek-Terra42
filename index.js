71faf9b129447b8fed909050626bdada2c167810
Petra Zemanová <zemape42@gmail.com>
2020-05-08 17:24:07 +0200

    Hotový úkol

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
 
 console.log('funguju!');
+
+
+const tasks = [];
+
+const updateTasks = () => {
+  const tasksElm = document.querySelector('.todo__tasks');
+  tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    tasksElm.innerHTML += `<div class='task'>${tasks[i]}</div>`;
+  }
+}
+
+const addTask = () => {
+  const newTask = document.querySelector('#new-task');
+  tasks.push(newTask.value);
+  updateTasks();
+  newTask.value = '';
+};
+
+const btnElm = document.querySelector('.btn-add');
+btnElm.addEventListener('click', addTask);
